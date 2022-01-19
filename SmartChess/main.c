c584fd8f1a8c7ed0b7a710a4fdd7806e3f110ef4
MarcosGB <marcosgarralaga@gmail.com>
2022-01-19 17:59:13 +0000

    Important bugfixes to main

--- SmartChess/main.c
+++ SmartChess/main.c
@@ -8,7 +8,7 @@
  *	Display of the battery of the board
  */
 
-#define DEBUG_MODE 1
+#define DEBUG_MODE 0
 #ifndef F_CPU
 	#define F_CPU 8000000UL
 #endif
@@ -18,109 +18,92 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <avr/power.h>
 #include "piece.h"
 #include "board.h"
 #include "usart.h"
 
-const uint8_t MASKS[8] = {(1>>PINB1), (1>>PB2), (1>>PC0), (1>>PC1), (1>>PC2), (1>>PC3), (1>>PC4), (1>>PC5)};
 
 //Pre: "output" is an array of at least 8 chars in a known state.
 //Post: The row read data is saved to the array
 void readRow(unsigned char output[]) {
-	unsigned char buffer[20] = "";
-	itoa((PINB & MASKS[0]),buffer,2);
-	USART_transmit_str(buffer);
-	if ((PINB & MASKS[0]) == 0) {
+	if (PINB & (1<<PINB1)) {
 		output[0] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[0] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINB & MASKS[1]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINB & MASKS[1]) == 0) {
+	if (PINB & (1<<PINB2)) {
 		output[1] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[1] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[2]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[2]) == 0) {
+	if (PINC & (1<<PINC0)) {
 		output[2] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[2] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[3]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[3]) == 0) {
+	if (PINC & (1<<PINC1)) {
 		output[3] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[3] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[4]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[4]) == 0) {
+	if (PINC & (1<<PINC2)) {
 		output[4] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[4] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[5]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[5]) == 0) {
+	if (PINC & (1<<PINC3)) {
 		output[5] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[5] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[6]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[6]) == 0) {
+	if (PINC & (1<<PINC4)) {
 		output[6] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[6] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 	
-	itoa((PINC & MASKS[7]),buffer,2);
-	USART_transmit_str(buffer);
 	
-	if ((PINC & MASKS[7]) == 0) {
+	if (PINC & (1<<PINC5)) {
 		output[7] = 0;
-		//USART_block_transmit('0');
+		USART_block_transmit('0');
 	}
 	else {
 		output[7] = 1;
-		//USART_block_transmit('X');
+		USART_block_transmit('X');
 	}
 
 	USART_block_transmit('\n');
@@ -214,13 +197,12 @@ int main(void) {
 	 */
 	DDRD |= (1<<PD3) | (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7);
 	DDRB |= (1<<PB6) | (1<<PB7) | (1<<PB0);
+	
 	DDRB |= (1<<PB5);
 	PORTB |= (1<<PB5);
+	
 	USART_init(UBRR);
 	prepareGame();
-	EIMSK |= (1<<INT0); //Enable external interruptions on pin INT0 (=PD2)
-	EICRA |= (1<<ISC11); //Falling edge on INT0 generates an interrupt request.
-	sei();
 	if (DEBUG_MODE == 1) {
 		_delay_ms(2000);
 		USART_transmit_str("BEGIN DEBUG");
@@ -230,6 +212,12 @@ int main(void) {
 			readBoard(detected_position);
 		}
 	}
+	
+	
+	
+	EIMSK |= (1<<INT0); //Enable external interruptions on pin INT0 (=PD2)
+	EICRA |= (1<<ISC11); //Falling edge on INT0 generates an interrupt request.
+	sei();
 	
 	char r = '#';
 	while(1)
